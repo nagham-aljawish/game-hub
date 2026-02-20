bfc31beeac29517d84a460e737abac99f043426f
Nagham Aljawish <nagham.aljawish@gmail.com>
2026-02-21 00:04:40 +0300

    Create a genreic data fetching hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+
+import useData from "./useData";
 
 export interface Platform{
   id: number;
@@ -14,34 +13,8 @@ export interface Game {
   parent_platforms:{platform:Platform}[];
   metacritic: number;
 }
-interface FetchGamesRespnses {
-  count: number;
-  results: Game[];
-}
 
-const useGames = () =>{
-     const [games, setGames] = useState<Game[]>([]);
-      const [error, setError] = useState("");
-      const[isLoading, setIsLoading]= useState(false);
+
+const useGames = () => useData<Game>("/games");
     
-      useEffect(() => {
-        const controller = new AbortController();
-        setIsLoading(true);
-        apiClient
-          .get<FetchGamesRespnses>("/games",{signal:controller.signal})
-          .then((res) => {
-            
-            setGames(res.data.results)
-             setIsLoading(false);
-          })
-          .catch((err) =>{ 
-            if( err instanceof CanceledError) return;
-            setError(err.message);
-             setIsLoading(true);
-          });
-
-          return ()=> controller.abort();
-      },[]);
-      return{games,error, isLoading};
-};
 export default useGames;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,38 +1,11 @@
-import { CanceledError } from "axios";
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
 
-interface Genre {
+import useData from "./useData";
+
+export interface Genre {
   id: number;
   name: string;
 }
-interface FetchGenreRespnses {
-  count: number;
-  results: Genre[];
-}
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    setIsLoading(true);
-    apiClient
-      .get<FetchGenreRespnses>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setIsLoading(true);
-      });
-
-    return () => controller.abort();
-  }, []);
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>('/genres');
 
 export default useGenres;
