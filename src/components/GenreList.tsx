de552aa668265391c4e340655640d56fb2557021
Nagham Aljawish <nagham.aljawish@gmail.com>
2026-02-24 15:51:10 +0300

    Highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -11,9 +11,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectedGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectedGenre }: Props) => {
+const GenreList = ({ onSelectedGenre , selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner />;
@@ -31,6 +32,7 @@ const GenreList = ({ onSelectedGenre }: Props) => {
               variant={"link"}
               onClick={() => onSelectedGenre(genre)}
               fontSize={"lg"}
+              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
             >
               {genre.name}
             </Button>
