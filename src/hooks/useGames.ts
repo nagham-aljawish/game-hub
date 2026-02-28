5a4634bc5bc54f8bdc775e41c377f1817971615d
Nagham Aljawish <nagham.aljawish@gmail.com>
2026-02-28 16:46:50 +0300

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -19,7 +19,9 @@ export interface Game {
 
 const useGames = (gameQuery:GameQuery) => useData<Game>("/games",
   {params:{genres:gameQuery.genre?.id ,
-   platforms:gameQuery.platform?.id}},
-  [gameQuery.genre?.id, gameQuery.platform?.id]);
+   platforms:gameQuery.platform?.id ,
+   ordering:gameQuery.sortOrder,
+  }},
+  [gameQuery.genre?.id, gameQuery.platform?.id , gameQuery?.sortOrder]);
     
 export default useGames;
